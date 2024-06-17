c4fa565ffd8da066b9cc987d40717e56f7de8e84
jpmillz <legitmillz@gmail.com>
2024-06-17 16:15:53 -0500

    -Added andromlite paxel and sword Textures: -Added textures for the andromalite paxel and sword DataGen: -Added data gen for item models -Added datagen for andromalite tier ingredient in block tags

---
+++ src/main/java/io/github/jpmillz/paracausality/core/init/tiers/ModTiers.java
@@ -0,0 +1,15 @@
+package io.github.jpmillz.paracausality.core.init.tiers;
+
+import io.github.jpmillz.paracausality.core.data.util.ModTags;
+import io.github.jpmillz.paracausality.core.init.ItemInit;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.neoforged.neoforge.common.SimpleTier;
+
+public class ModTiers {
+    public static final Tier ANDROMALITE = new SimpleTier(5, 5400, 12.0f,4.5f,22, ModTags.Blocks.NEEDS_ANDROMALITE_TOOL,
+            ()-> Ingredient.of(ItemInit.ANDROMALITE_INGOT.get()));
+
+
+
+}
